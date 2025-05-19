5952e349da49b6b93a87091ef31b4611e75866ab
LYJ0304 <omlmg0304@naver.com>
2025-05-19 22:48:27 +0900

    HTTP와 String

--- src/main/java/com/ll/demo_01/HomeController.java
+++ src/main/java/com/ll/demo_01/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.demo_01;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,14 +10,25 @@ public class HomeController {
 
     @GetMapping("a") // http://localhost:8090/a
     @ResponseBody
-    public String hello() {
-        return "Hello";
+    public String hello(
+            String age,
+            String id
+    ) {
+        return "%s번 사람의 나이는 %s살 입니다.".formatted(id, age);
     }
 
     @GetMapping("b")
     @ResponseBody
-    public String hello2() {
-        return "안녕하세요.";
+    public String plus(
+            @RequestParam("a") String num1Str,
+            @RequestParam("b")String num2Str
+    ) {
+        int num1 = Integer.parseInt(num1Str);
+        int num2 = Integer.parseInt(num2Str);
+        System.out.println("a = " + num1Str);
+        System.out.println("b = " + num2Str);
+
+        return "a + b = %d".formatted(num1 + num2);
     }
 
 }
