bd4219dc8d7317a2b098f44ab93bcfdde3a32aa1
LYJ0304 <omlmg0304@naver.com>
2025-05-19 23:16:48 +0900

    Jackson과 JSON 파싱

--- src/main/java/com/ll/demo_01/HomeController.java
+++ src/main/java/com/ll/demo_01/HomeController.java
@@ -20,15 +20,87 @@ public class HomeController {
     @GetMapping("b")
     @ResponseBody
     public String plus(
-            @RequestParam("a") String num1Str,
-            @RequestParam("b")String num2Str
+            @RequestParam("a") int num1,
+            @RequestParam("b") int num2,
+            @RequestParam(name = "c", defaultValue = "0") int num3
     ) {
-        int num1 = Integer.parseInt(num1Str);
-        int num2 = Integer.parseInt(num2Str);
-        System.out.println("a = " + num1Str);
-        System.out.println("b = " + num2Str);
+        System.out.println("a = " + num1);
+        System.out.println("b = " + num2);
+        System.out.println("c = " + num3);
 
-        return "a + b = %d".formatted(num1 + num2);
+        return "a + b  + c= %d".formatted(num1 + num2 + num3);
     }
 
+    @GetMapping("c")
+    @ResponseBody
+    public String c(
+            boolean married
+    ) {
+        return married ? "결혼" : "미혼";
+    }
+
+    public static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+
+    @GetMapping("person")
+    @ResponseBody
+    public String person(
+            String name,
+            int age
+    ) {
+        Person person = new Person(name, age);
+
+        return person.toString();
+    }
+
+    @GetMapping("person2")
+    @ResponseBody
+    public String person2(
+           Person person
+    ) {
+        return person.toString();
+    }
+
+    @GetMapping("e")
+    @ResponseBody
+    public int e() {
+        return 10;
+    }
+
+    @GetMapping("f")
+    @ResponseBody
+    public boolean f() {
+        return true;
+    }
 }
